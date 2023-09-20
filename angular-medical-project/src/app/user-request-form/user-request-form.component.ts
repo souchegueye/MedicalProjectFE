67c2670f5b2ac6db69ec00a2b2527bb875724335
edwin <edwin.lingson@gmail.com>
2023-09-20 12:57:40 -0400

    User Request Form

---
+++ angular-medical-project/src/app/user-request-form/user-request-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-request-form',
+  templateUrl: './user-request-form.component.html',
+  styleUrls: ['./user-request-form.component.css']
+})
+export class UserRequestFormComponent {
+
+}
