4e49cbfcf56459115eacba1ce246c9a414a32d0a
souchegueye <gueyemomo24@gmail.com>
2023-09-20 15:35:57 -0400

    Class Doctor is added

---
+++ angular-medical-project/src/app/doctor/doctor.ts
@@ -0,0 +1,11 @@
+export class Doctor {
+  id: number;
+  name: string;
+  specialty: string;
+
+  constructor(id: number, name: string, specialty: string) {
+    this.id = id;
+    this.name = name;
+    this.specialty = specialty;
+  }
+}
