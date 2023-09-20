05aa8c8838b4b7beda7987b052cd406803b344fa
souchegueye <gueyemomo24@gmail.com>
2023-09-20 16:31:31 -0400

    Class Doctor is modified

--- angular-medical-project/src/app/doctor/doctor.ts
+++ angular-medical-project/src/app/doctor/doctor.ts
@@ -1,11 +1,36 @@
 export class Doctor {
   id: number;
+  uname: string;
+  pwd: string;
+  specialization: string;
   name: string;
-  specialty: string;
+  fee: number;
+  experience: number;
+  gender: string;
+  availableFrom: string;
+  availableTo: string;
 
-  constructor(id: number, name: string, specialty: string) {
+  constructor(
+    id: number,
+    uname: string,
+    pwd: string,
+    specialization: string,
+    name: string,
+    fee: number,
+    experience: number,
+    gender: string,
+    availableFrom: string,
+    availableTo: string
+  ) {
     this.id = id;
+    this.uname = uname;
+    this.pwd = pwd;
+    this.specialization = specialization;
     this.name = name;
-    this.specialty = specialty;
+    this.fee = fee;
+    this.experience = experience;
+    this.gender = gender;
+    this.availableFrom = availableFrom;
+    this.availableTo = availableTo;
   }
 }
