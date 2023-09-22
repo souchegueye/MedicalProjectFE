1584eb513d71b432e95459d51b4afeb52b92c8a8
ramyaperam <peram.ramya77@gmail.com>
2023-09-22 12:29:27 -0500

    add doctor

--- angular-medical-project/src/app/app-routing.module.ts
+++ angular-medical-project/src/app/app-routing.module.ts
@@ -1,47 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminLandingPageComponent } from './admin-landing-page/admin-landing-page.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminRegistrationComponent } from './admin-registration/admin-registration.component';
-
+import { DeleteDoctorComponent } from './delete-doctor/delete-doctor.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { UserRequestFormComponent } from './user-request-form/user-request-form.component';
-import { ListProbableDoctorsComponent } from './list-probable-doctors/list-probable-doctors.component';
-import { ListSelectedDoctorComponent } from './list-selected-doctor/list-selected-doctor.component';
+import { UpdateDoctorComponent } from './update-doctor/update-doctor.component';
+import { ViewAppointmentsComponent } from './view-appointments/view-appointments.component';
+import { ViewDoctorComponent } from './view-doctor/view-doctor.component';
 
-import { ViewOrderComponent } from './view-order/view-order.component';
-import { ApptResultComponent } from './appt-result/appt-result.component';
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'view-order', component: ViewOrderComponent },
   { path: 'order-details', component: AddDoctorComponent },
-  { path: 'admin', component: AdminLandingPageComponent }, 
-  { path: 'admin/registration', component: AdminRegistrationComponent }, 
-  { path: 'admin/login', component: AdminLoginComponent }, 
-
-  { path: '', component: HomePageComponent },
-
+  { path: 'admin-landing-page', component: AdminLandingPageComponent },
   { path: 'admin-registration', component: AdminRegistrationComponent },
   { path: 'admin-login', component: AdminLoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
   { path: 'add-doctor', component: AddDoctorComponent },
-  { path: 'user-details', component: UserRequestFormComponent },
-  {
-    path: 'list-selected-doctors/:spec',
-    component: ListProbableDoctorsComponent,
-  },
-  { path: 'selectedDoctor', component: ListSelectedDoctorComponent },
-  {
-    path: 'list-selected-doctors/:spec',
-    component: ListProbableDoctorsComponent,
-  },
-  { path: 'selectedDoctor', component: ListSelectedDoctorComponent },
-  { path: 'apptResult', component: ApptResultComponent },
-
+  { path: 'view-doctor', component: ViewDoctorComponent },
+  { path: 'update-doctor', component: UpdateDoctorComponent },
+  { path: 'delete-doctor', component: DeleteDoctorComponent },
+  { path: 'view-appointments', component: ViewAppointmentsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule], 
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
